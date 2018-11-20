7ee36c95c2198c8fe1e330ab5bfa31531c1e4d73
gaocheng <gaocheng@rd.netease.com>
2018-11-20 14:19:49 +0800

    解决dataTree无法加载问题

--- src/components/menu/menuTree.vue
+++ src/components/menu/menuTree.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
   <Menu theme="dark" active-name="1">
-    <div v-if="dataTreeNodes === ''">
-          <submenu v-for="item in dataTreeNodes" :model="item"></submenu>
+    <div v-if="dataTreeNodes">
+          <submenu v-for="item in dataTreeNodes" :model="item" ></submenu>
     </div>
     <div v-else>
         <h1 style="color:red">暂无数据</br>请联系管理员！</h1>
@@ -14,23 +14,17 @@
 <script>
 import submenu from '@/components/menu/submenu.vue';
 import { mapState, mapActions } from 'vuex'
+import store from '@/store.js'
 import axios from 'axios'
 export default {
-     components:{submenu},
-// data(){
-//  return {
-//     dateTreeNodes:'hello',
-//         }
-//     },
+    components:{submenu},
     computed:{
-    //   getDataTree: function () {
-    //     return axios.get('/api/data-tree').then( res => {
-    //     this.DataTree = res.data.treeNodes
-    //   })
-    //  }
-     ...mapState({
+         ...mapState({
       dataTreeNodes: "dataTreeNodes"
     })
-    }
+    },
+     data(){
+            return this.$store.dispatch('reloadDataTree')
+          }
 }
 </script>
