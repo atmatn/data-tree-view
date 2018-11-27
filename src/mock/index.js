2f01885a7014e21ee552b1ab211f53123d75afd1
Sining Li <lisn@rd.netease.com>
2018-11-27 14:21:00 +0800

    MOD: add 'currentUserManageable' and 'creator' to data tree node

--- src/mock/index.js
+++ src/mock/index.js
@@ -21,6 +21,7 @@ var getDataTree = function getDataTree ({ url, type, body }) {
         visible_perms: ['ke_general'], // product的perms是“可见”权限，有该权限则所有子节点可见
         currentUserVisible: true, //  （后端计算出的属性）当前用户是否有“可见”权限
         containsExecutableForCurrentUser: true, // （后端计算出的属性）本节点或其子孙节点是否含有当前用户可执行的叶子节点
+        creator: 'bob', // 创建者
         children: [
           {
             type: 'folder',
@@ -29,6 +30,8 @@ var getDataTree = function getDataTree ({ url, type, body }) {
             currentUserExecutable: true,
             computed_executable_perms: ['ke_general'], // （后端计算出的属性，如果没有配置，会直接拷贝product的visible_perms）folder的perms是“执行”权限；前端可以提示用户，需要该权限（之一）才能执行
             containsExecutableForCurrentUser: true,
+            currentUserManageable: true, // （后端计算出的属性，创建者或者与“manage_perms”相符的 currentUserManageable 才为 true）
+            creator: 'bob', // 创建者
             children: [
               {
                 type: 'direct-link',
@@ -37,6 +40,8 @@ var getDataTree = function getDataTree ({ url, type, body }) {
                 currentUserExecutable: true,
                 containsExecutableForCurrentUser: true, // （后端计算出的属性）
                 computed_executable_perms: ['ke_general'], // （后端计算出的属性，如果没有配置，会直接拷贝父节点(folder)的computed_executable_perms）folder的perms是“执行”权限；前端可以提示用户，需要该权限（之一）才能执行
+                currentUserManageable: true, // （后端计算出的属性）
+                creator: 'bob',
                 linkUrl: 'http://analyzer2.corp.youdao.com/'
               },
               {
@@ -45,7 +50,9 @@ var getDataTree = function getDataTree ({ url, type, body }) {
                 title: '绝密KPI数据',
                 currentUserExecutable: false,
                 containsExecutableForCurrentUser: false, // （后端计算出的属性）
-                computed_executable_perms: ['ke_core'] // （后端计算出的属性，如果没有配置，会直接拷贝父节点(folder)的computed_executable_perms）folder的perms是“执行”权限；前端可以提示用户，需要该权限（之一）才能执行
+                computed_executable_perms: ['ke_core'], // （后端计算出的属性，如果没有配置，会直接拷贝父节点(folder)的computed_executable_perms）folder的perms是“执行”权限；前端可以提示用户，需要该权限（之一）才能执行
+                currentUserManageable: false, // （后端计算出的属性）
+                creator: 'mary'
                 // （后端剥离掉的数据）用户没有执行权限，则后端不提供对应的linkUrl
                 // linkUrl: '/xxx.html',
               }
@@ -56,6 +63,8 @@ var getDataTree = function getDataTree ({ url, type, body }) {
             id: 5,
             title: '财务',
             computed_executable_perms: ['ke_financial'], // （后端计算出的属性，如果没有配置，会直接拷贝product的visible_perms）folder的perms是“执行”权限；前端可以提示用户，需要该权限（之一）才能执行
+            currentUserManageable: true, // （后端计算出的属性）
+            creator: 'bob',
             children: [
               {
                 type: 'args-script',
@@ -64,6 +73,8 @@ var getDataTree = function getDataTree ({ url, type, body }) {
                 currentUserExecutable: true, // （后端计算出的属性）当前用户是否有“执行”权限
                 containsExecutableForCurrentUser: true, // （后端计算出的属性）
                 computed_executable_perms: ['ke_financial'], // （后端计算出的属性，如果没有配置，会直接拷贝folder的computed_executable_perms）args-script的perms是“执行”权限；前端可以提示用户，需要该权限（之一）才能执行
+                currentUserManageable: true, // （后端计算出的属性）
+                creator: 'bob',
                 scriptId: '123',
                 scriptParams: {
                   param_a: 1,
@@ -76,7 +87,9 @@ var getDataTree = function getDataTree ({ url, type, body }) {
                 title: '实用英语概要数据',
                 currentUserExecutable: false, // （后端计算出的属性）当前用户是否有“执行”权限
                 containsExecutableForCurrentUser: false, // （后端计算出的属性）
-                computed_executable_perms: ['ke_chief_financial'] // （后端计算出的属性）args-script的perms是“执行”权限；前端可以提示用户，需要该权限才能执行
+                computed_executable_perms: ['ke_chief_financial'], // （后端计算出的属性）args-script的perms是“执行”权限；前端可以提示用户，需要该权限才能执行
+                currentUserManageable: false, // （后端计算出的属性）
+                creator: 'sammy'
                 // （后端剥离掉的数据）用户没有执行权限，则后端不提供对应的scriptId和params
                 // scriptId: '123',
                 // scriptParams: {
@@ -90,6 +103,11 @@ var getDataTree = function getDataTree ({ url, type, body }) {
             type: 'folder',
             id: 6,
             title: '市场',
+            currentUserExecutable: true, // （后端计算出的属性）当前用户是否有“执行”权限
+            computed_executable_perms: ['ke_general'],
+            currentUserManageable: true, // （后端计算出的属性）
+            containsExecutableForCurrentUser: true, // （后端计算出的属性）
+            creator: 'bob',
             children: [
               {
                 type: 'args-script',
@@ -98,6 +116,8 @@ var getDataTree = function getDataTree ({ url, type, body }) {
                 currentUserExecutable: true, // （后端计算出的属性）
                 containsExecutableForCurrentUser: true, // （后端计算出的属性）
                 computed_executable_perms: ['ke_chief_financial'], // （后端计算出的属性）args-script的perms是“执行”权限；前端可以提示用户，需要该权限才能执行
+                currentUserManageable: true, // （后端计算出的属性）
+                creator: 'bob',
                 scriptId: '456',
                 scriptParams: {
                   param_a: 4,
@@ -113,6 +133,8 @@ var getDataTree = function getDataTree ({ url, type, body }) {
             currentUserExecutable: true, // （后端计算出的属性）
             containsExecutableForCurrentUser: false, // （后端计算出的属性）为false是因为本节点目前是空的，不包含可执行的叶子节点
             computed_executable_perms: ['ke_general'], // （后端计算出的属性）
+            currentUserManageable: true, // （后端计算出的属性）
+            creator: 'bob',
             children: [
               // 空的
             ]
