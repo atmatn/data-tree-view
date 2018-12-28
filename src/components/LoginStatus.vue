5c80e9b1a8b59bd0bd47e2cdcfa3550b92bc1835
Sining Li <lisn@rd.netease.com>
2018-12-28 17:20:00 +0800

    ADD: 遗漏的文件

---
+++ src/components/LoginStatus.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-status">
+    用户: {{loginStatus.username}} <Tooltip content="登出"><a href="/logout"><Icon type="md-exit" /></a></Tooltip>
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+export default {
+  computed: {
+    ...mapState(['loginStatus'])
+  }
+}
+</script>
+
+<style scoped lang="less">
+.login-status{
+  color: white;
+}
+
+</style>
